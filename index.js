6ac6c7a8ccd7f733bd377b93285f0a96e11d197a
Khaled Mohsen <khaledmohsenmohammed@gmail.com>
2025-02-20 15:55:34 +0200

    Heroku deployment fix command in js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
-const port = 3000;
+
+// Use the PORT environment variable or fallback to 3000 for local development
+const PORT = process.env.PORT || 3000;
 
 // In-memory storage for used emails
 const usedEmails = new Set();
@@ -9,7 +11,7 @@ const usedEmails = new Set();
 // Function to generate a random email
 function generateRandomEmail() {
   const randomString = uuidv4().split('-')[0]; // Take the first part of the UUID
-  const email = `${randomString}@kmmtest.com`;
+  const email = `${randomString}@example.com`;
   return email;
 }
 
@@ -25,6 +27,6 @@ app.get('/get-email', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
